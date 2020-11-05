d3aabedcfe603e1fd969817f9b24f16e243fd3bf
cuiwan <328249403@qq.com>
2020-11-05 21:52:37 +0800

    EasyExcle工具类 复杂模板导出Excel

--- src/main/java/com/demo/DockerController.java
+++ src/main/java/com/demo/DockerController.java
@@ -1,6 +1,9 @@
 package com.demo;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
 import com.demo.utils.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class DockerController {
@@ -63,6 +64,34 @@ public class DockerController {
                 .doFill(students);
         log.info("importExcel success!");
         return "importExcel success!";
+    }
+    @RequestMapping("/importModeExcel")
+    public String importModeExcel() throws Exception {
+        String path = ResourceUtils.getURL("").getPath();
+        path = (path.substring(1,path.length())+"src/main/resources/").replace("/", File.separator);
+        String fileName =  path+"demomode.xls";
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            students.add(new Student(i+"","张三"+i, StringUtils.getIdNo(false),i,(i%2)==1?"男":"女"));
+        }
+        // 设置输出目标和模板，out和template 可以是文件路径或流
+        ExcelWriter excelWriter = EasyExcel.write(path+ Calendar.getInstance().getTimeInMillis()+".xls")
+                .withTemplate(fileName)
+                .build();
+        // 创建 Sheet
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        // 这里注意 入参用了forceNewRow 代表在写入list的时候不管list下面有没有空行 都会创建一行，然后下面的数据往后移动。
+        // 默认 是false，会直接使用下一行，如果没有则创建。但是这个就会把所有数据放到内存 会很耗内存
+        FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
+        excelWriter.fill(students, fillConfig, writeSheet);
+        // 填充普通变量
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("className", "软件工程3班");
+        map.put("date", "2020年1月5号");
+        excelWriter.fill(map, writeSheet);
+        excelWriter.finish();
+        log.info("importModeExcel success!");
+        return "importModeExcel success!";
     }
 
 }
